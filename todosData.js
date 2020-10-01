cdf6aff0038a8ba90a347e3028c10a8fcd7ed2b7
Ashlyn Hanson <44590338+AshlynHanson@users.noreply.github.com>
2020-09-30 22:15:26 -0500

    Add files via upload

---
+++ todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Make bed",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Do homework",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Watch Harry Potter",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Take out trash",
+        completed: false
+    }
+]
+
+export default todosData
